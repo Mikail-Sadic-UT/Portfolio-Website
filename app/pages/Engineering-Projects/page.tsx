8b5c2f50ef4144d628b6b46bd106a2c0a6d41772
Mikail Sadic <mikail.sadic@utexas.edu>
2024-02-25 01:59:32 -0600

    0.77
    Updated Engineering Project

--- app/pages/Engineering-Projects/page.tsx
+++ app/pages/Engineering-Projects/page.tsx
@@ -30,8 +30,13 @@ export default function Page() {
         <p className={styles.projText}>The final project for my ECE-319K (Intro to Embedded Systems) class, which is a game built on C and running off of the TI-M0+ microcontroller.</p>
         </div>
         <div>
-        <h2 className={styles.projName}>El Robot | 2024</h2>
-        <p className={styles.projText}>A project I'm working on with a group of friends that's in the planning stage currently.</p>
+        <h2 className={styles.projName}>FPV RC Car | 2024</h2>
+        <p className={styles.projText}>A project that I've organized and started up with a group of friends, with the goal of creating an FPV VR RC Car. The plan is to build it from the ground up as much as we can, from CAD, to PCBs, to Controllers, to Software. The idea is to drive the RC car in VR with a virtual wheel and real-time video feedback.</p>
+        <p className={styles.ghubText}>Github:{" "}
+          <Link className={styles.projLink} href="https://github.com/Basement-Builders-Club" target="_blank" rel="noopener noreferrer">
+          https://github.com/Basement-Builders-Club
+          </Link>
+        </p>
         </div>
       </div>
     </main>
