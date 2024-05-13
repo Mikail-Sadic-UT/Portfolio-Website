ba497398a93c674868f0d88b5356a4a146c65779
Mikail Sadic <mikail.sadic@utexas.edu>
2024-05-13 19:31:37 +0200

    v0.78
    Updated engineering projects

--- app/pages/Engineering-Projects/page.tsx
+++ app/pages/Engineering-Projects/page.tsx
@@ -17,7 +17,7 @@ export default function Page() {
       <div className={styles.title}>
         <h1 className={styles.mainTitle}>Engineering Projects</h1>
         <div>
-        <h2 className={styles.projName}>Portfolio Website | Jan-Feb 2024</h2>
+        <h2 className={styles.projName}>Portfolio Website | Jan 2024</h2>
         <p className={styles.projText}>My first main engineering project that I'm tackling to use for future projects I plan on doing. Built on Next.js (a React framework), HTML and CSS.</p>
         <p className={styles.ghubText}>Github:{" "}
           <Link className={styles.projLink} href="https://github.com/Mikail-Sadic-UT/Portfolio-Website" target="_blank" rel="noopener noreferrer">
@@ -26,8 +26,21 @@ export default function Page() {
         </p>
         </div>
         <div>
-       <h2 className={styles.projName}>Microcontroller Game | Apr 2024 (Future)</h2>
+       <h2 className={styles.projName}>Microcontroller Game | Apr 2024</h2>
         <p className={styles.projText}>The final project for my ECE-319K (Intro to Embedded Systems) class, which is a game built on C and running off of the TI-M0+ microcontroller.</p>
+        <p className={styles.projText}>The game I made is a bullet-hell game, where the objective is to dodge the bullets shot from the boss while shooting back. 
+        The game's main points are its difficulty, smoothness despite the number of entities on the screen, and the bullet patterns and boss phases themselves.</p>
+        <p className={styles.projText}>This game was the winner of my class competition, and came 5th overall in the combined class competition.</p>
+        <p className={styles.ghubText}>Youtube:{" "}
+          <Link className={styles.projLink} href="https://www.youtube.com/watch?v=lAslunfPq5c" target="_blank" rel="noopener noreferrer">
+          https://www.youtube.com/watch?v=lAslunfPq5c
+          </Link>
+        </p>
+        <p className={styles.ghubText}>Github:{" "}
+          <Link className={styles.projLink} href="https://github.com/Mikail-Sadic-UT/ECE-319K-Labs/tree/main/Lab9" target="_blank" rel="noopener noreferrer">
+          https://github.com/Mikail-Sadic-UT/ECE-319K-Labs/tree/main/Lab9
+          </Link>
+        </p>
         </div>
         <div>
         <h2 className={styles.projName}>FPV RC Car | 2024</h2>
