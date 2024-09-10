da5f050979fb33dc7405b18c7d6f4216848c8e63
Mikail Sadic <mikail.sadic@utexas.edu>
2024-09-09 23:41:19 -0500

    fixed typo :p

--- app/pages/Engineering-Projects/page.tsx
+++ app/pages/Engineering-Projects/page.tsx
@@ -30,7 +30,7 @@ export default function Page() {
         <p className={styles.projText}>The final project for my ECE-319K (Intro to Embedded Systems) class, which is a game built on C and running off of the TI-M0+ microcontroller.</p>
         <p className={styles.projText}>I made a bullet-hell style game, PASS//FAIL, where the objective is to dodge large numbers of bullets shot from the boss while shooting back. 
         The game's main points are its difficulty, smoothness despite the number of entities on the screen, and the bullet patterns and boss phases themselves.
-        It is entirely writted in C, consisting of 1,800+ lines of code, including custom driver implementation. There was also extensive player testing to fine-tune 
+        It is entirely written in C, consisting of 1,800+ lines of code, including custom driver implementation. There was also extensive player testing to fine-tune 
         controls, difficulty, and overall user experience.</p>
         <p className={styles.projText}>This game was the winner of my class competition, and came 5th overall in the combined class competition.</p>
         <p className={styles.ghubText}>Youtube:{" "}
